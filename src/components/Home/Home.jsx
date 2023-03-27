f21ffa89a22b2a81b2d745d0223c01727700bef9
luichgar <luisgg.cm@gmail.com>
2023-03-26 18:15:02 -0600

    v1.0

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainPokemonCard from '../MainPokemonCard/MainPokemonCard'
+
+export default function Home() {
+  return (
+    <div>
+    <MainPokemonCard/>
+    </div>
+  )
+}
