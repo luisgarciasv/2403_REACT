c9d94ee26bddfea778beb0fc3b02add5b11e4c2a
luisgarciasv <luisgarciasv3@gmail.com>
2025-10-13 13:21:20 -0600

    chore: format with prettier

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,56 +1,60 @@
-import { useState, useContext } from 'react'
-import { bgContext } from '../../App'
-import Filter from '../Filter/Filter'
-import MainPokemonCard from '../MainPokemonCard/MainPokemonCard'
-import Seach from '../Search/Seach'
-import Footer from '../Footer/Footer'
-import searchIcon from '../img/search-icon.svg'
-import filterIcon from '../img/filter-icon.svg'
+import { useState, useContext } from "react";
+import { bgContext } from "../../App";
+import Filter from "../Filter/Filter";
+import MainPokemonCard from "../MainPokemonCard/MainPokemonCard";
+import Search from "../Search/Search";
+import Footer from "../Footer/Footer";
+import searchIcon from "../img/search-icon.svg";
+import filterIcon from "../img/filter-icon.svg";
 
 export default function Home() {
-
   const [contextBg, setContextBg] = useContext(bgContext);
-  const [renderSearch , setRenderSearch] = useState(false);
-  const [renderFilter , setRenderFilter] = useState(false);
+  const [renderSearch, setRenderSearch] = useState(false);
+  const [renderFilter, setRenderFilter] = useState(false);
 
   const handleClickSearch = () => {
-  
-      setRenderSearch(true);
-      setRenderFilter(false);
-      setContextBg(null)
-    
-  }
+    setRenderSearch(true);
+    setRenderFilter(false);
+    setContextBg(null);
+  };
 
   const handleClickFilter = () => {
-    
-      setRenderFilter(true);
-      setRenderSearch(false);
-      setContextBg(null)
-    
-  }
+    setRenderFilter(true);
+    setRenderSearch(false);
+    setContextBg(null);
+  };
 
   return (
-    <div  >
-      <div className="home-header" >
-        <h1 className='main-title'>Pokedex</h1>
-        <img src={searchIcon} 
-        alt="search icon" 
-        role="button"
-        title='Search'
-        onClick={handleClickSearch}/>
-        <img src={filterIcon} 
-        alt="filter icon"
-        role="button"
-        title='Filter'
-        onClick={handleClickFilter}/>
+    <div>
+      <div className="home-header">
+        <h1 className="main-title">Pokedex</h1>
+        <img
+          src={searchIcon}
+          alt="search icon"
+          role="button"
+          title="Search"
+          onClick={handleClickSearch}
+        />
+        <img
+          src={filterIcon}
+          alt="filter icon"
+          role="button"
+          title="Filter"
+          onClick={handleClickFilter}
+        />
       </div>
       <div className="card">
-      {renderSearch ? <><Seach/> <MainPokemonCard/></> : <></>}
-      {renderFilter ? <Filter/> : <></>}
-      
+        {renderSearch ? (
+          <>
+            <Search /> <MainPokemonCard />
+          </>
+        ) : (
+          <></>
+        )}
+        {renderFilter ? <Filter /> : <></>}
       </div>
-  
-     <Footer/>
+
+      <Footer />
     </div>
-  )
+  );
 }
